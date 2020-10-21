028bd21b1c8d6f077c63e1815474894247622f81
Fernando Calmet <fercalmet@gmail.com>
2020-10-21 13:52:50 -0500

    implemen crud

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rest-api-nodejs
-Rest API: Utilizando las tecnologías: NodeJS, MySQL. Además, se utilizo otras herramientas adicionales como: Express, Sequelize
+# Rest API with NodeJS
+
+Rest API: Utilizando las tecnologías: NodeJS, MySQL. Además, se utilizo otras herramientas adicionales como: Express, Sequelize.
+
+## :page_facing_up: LICENCIA
+
+Licencia MIT. Puedes verla en el [Archivo de Licencia](https://github.com/FernandoCalmet/rest-api-nodejs/blob/main/LICENSE) para más información.
+
+---
+
+:octocat: [Author](https://github.com/FernandoCalmet)
+
+[![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/T6T41JKMI)
--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -6,23 +6,104 @@ function save(req, res) {
     content: req.body.content,
     imageUrl: req.body.imageUrl,
     categoryId: req.body.categoryId,
-    userId: 1
+    userId: 1,
   };
 
   models.Post.create(post)
-    .then(result => {
+    .then((result) => {
       res.status(201).json({
         message: "Post created successfully",
-        post: result
+        post: result,
       });
-    }).catch((error) => {
+    })
+    .catch((error) => {
       res.status(500).json({
-        message: "Something went wrong",
-        error: error
+        message: "Something went wrong!",
+        error: error,
+      });
+    });
+}
+
+function show(req, res) {
+  const id = req.params.id;
+
+  models.Post.findByPk(id)
+    .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({
+          message: "Post not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong!",
+      });
+    });
+}
+
+function index(req, res) {
+  models.Post.findAll()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong!",
+      });
+    });
+}
+
+function update(req, res) {
+  const id = req.params.id;
+  const updatedPost = {
+    title: req.body.title,
+    content: req.body.content,
+    imageUrl: req.body.imageUrl,
+    categoryId: req.body.categoryId,
+  };
+
+  const userId = 1;
+
+  models.Post.update(updatedPost, { where: { id: id, userId: userId } })
+    .then((result) => {
+      res.status(200).json({
+        message: "Post updated successfully",
+        post: updatedPost,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong!",
+        error: error,
+      });
+    });
+}
+
+function destroy(req, res) {
+  const id = req.params.id;
+  const userId = 1;
+
+  models.Post.destroy({ where: { id: id, userId: userId } })
+    .then((result) => {
+      res.status(200).json({
+        message: "Post deleted sucessfully",
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Something went wrong!",
+        error: error,
       });
     });
 }
 
 module.exports = {
-  save: save
+  save: save,
+  show: show,
+  index: index,
+  update: update,
+  destroy: destroy,
 };
